30ec07a7200eebbba8c51bba5bf959e554a8eb23
TriCore Systems Team <tcs.sa1.mail@tricoresolutions.com>
2016-10-13 04:36:40 -0400

    tweaks

--- webapp/3partest.py
+++ webapp/3partest.py
@@ -4,44 +4,27 @@ from hp3parclient import client, exceptions
 username='juice'
 password='tcs_juice'
 vluns = []
+
 host='10.66.100.6'
 cl = client.HP3ParClient("https://%s:8080/api/v1" % host)
 cl.login(username, password)
 volumes1 = cl.getVolumes()['members']
-for vol in volumes1:
-    vol['ip']= host
 hosts1 = cl.getHosts()['members']
-for host in hosts1:
-    host['ip'] = host
 vluns1 = cl.getVLUNs()['members']
-for lun in vluns1:
-    lun['ip'] = host
 
 host2='10.62.100.6'
 cl2 = client.HP3ParClient("https://%s:8080/api/v1" % host2)
 cl2.login(username, password)
 volumes2 = cl2.getVolumes()['members']
-for vol in volumes2:
-    vol['ip']= host2
 hosts2 = cl2.getHosts()['members']
-for host in hosts2:
-    host['ip'] = host2
 vluns2 = cl2.getVLUNs()['members']
-for lun in vluns2:
-    lun['ip'] = host2
 
 host3 = '10.62.100.117'
 cl3 = client.HP3ParClient("https://%s:8080/api/v1" % host2)
 cl3.login(username, password)
 volumes3 = cl3.getVolumes()['members']
-for vol in volumes3:
-    vol['ip']= host3
 hosts3 = cl3.getHosts()['members']
-for host in hosts3:
-    host['ip'] = host3
 vluns3  = cl3.getVLUNs()['members']
-for lun in vluns3:
-    lun['ip'] = host3
 
 volume_data = volumes1+volumes2+volumes3
 host_data = hosts1+hosts2+hosts3
@@ -66,11 +49,9 @@ with open('JSON/3par_vol.json', 'w') as outfile:
         json.dump(volume_data, outfile)
 with open('JSON/3par_host.json', 'w') as outfile:
         json.dump(host_data, outfile)
-
-with open('JSON/3par_host.json') as data_file:
-        par3Host_data = json.load(data_file)
 with open('JSON/3par_vlun.json', 'w') as outfile:
     json.dump(vluns, outfile)
+
 cl.logout()
 cl2.logout()
 cl3.logout()
--- webapp/sample_vmware2.py
+++ webapp/sample_vmware2.py
@@ -10,6 +10,7 @@ import atexit
 import getpass
 import ssl
 
+
 def main():
    """
    Simple command-line program for listing the virtual machines on a system.
@@ -29,18 +30,51 @@ def main():
        repo_dict['vmhost'] = vm.runtime.host.name or ''
        repo_dict['ip'] =  '10.62.100.15'
        repo_dict['vmname'] = vm.config.name
+       try:
+         vm_usage = vm.resourcePool.summary.quickStats.guestMemoryUsage 
+         repo_dict['vm_usage'] = vm_usage/1024
+         vm_reserved = vm.resourcePool.summary.runtime.memory.reservationUsedForVm 
+         repo_dict['vm_reserved']=vm_reserved/1024/1024
+       except:
+         pass
        repo_dict['vmware_disklist'] = []
-       hardware = vm.config.hardware
-       for each_vm_hardware in hardware.device:
+
+       for device in vm.config.hardware.device:
+            if (device.key >= 2000) and (device.key < 3000):
+               tmp_dict = {}
+               if hasattr(device.backing, 'fileName'):
+                   tmp_dict = {}
+                   datastore = device.backing.datastore
+                   #print  (device)
+                   if datastore:
+                       tmp_dict['reponame'] = datastore.name
+                       tmp_dict['capacity']  = device.capacityInKB/1024/1024 # converting bytes to GB
+                       #tmp_dict['used_size'] = (datastore.summary.capacity - datastore.summary.freeSpace)/1024/1024/1024
+                       tmp_dict['disk'] = device.backing.fileName.split('/')[1]
+               else:
+                    tmp_dict['reponame'] = ''
+                    tmp_dict['disk'] = ''
+                    tmp_dict['capacity']= 0
+                    tmp_dict['used_size'] = 0
+               repo_dict['vmware_disklist'].append(tmp_dict)
+       result.append(repo_dict)
+
+
+       """for each_vm_hardware in hardware.device:
          if (each_vm_hardware.key >= 2000) and (each_vm_hardware.key < 3000):
            tmp_dict = {}
-           reponame = each_vm_hardware.backing.fileName.split(']')[0] 
-           tmp_dict['reponame'] = reponame[1:]
-           disk = each_vm_hardware.backing.fileName.split('/')[1]
-           tmp_dict['disk'] = disk 
-           tmp_dict['capacity'] = each_vm_hardware.capacityInKB/1024/1024
+           try:
+               reponame = each_vm_hardware.backing.fileName.split(']')[0] 
+               tmp_dict['reponame'] = reponame[1:]
+               disk = each_vm_hardware.backing.fileName.split('/')[1]
+               tmp_dict['capacity'] = each_vm_hardware.capacityInKB/1024/1024
+               tmp_dict['disk'] = disk
+           except:
+               tmp_dict['reponame'] = ''
+               tmp_dict['disk'] = '' 
+               tmp_dict['capacity']= 0
            repo_dict['vmware_disklist'].append(tmp_dict)
-       result.append(repo_dict)
+       result.append(repo_dict)"""
 
    si = SmartConnect(host='10.66.100.15',
                      user='svc-juice',
@@ -54,18 +88,49 @@ def main():
        repo_dict['vmhost'] = vm.runtime.host.name or ''
        repo_dict['ip'] = '10.66.100.15' 
        repo_dict['vmname'] = vm.config.name
+       try:
+         vm_usage = vm.resourcePool.summary.quickStats.guestMemoryUsage
+         repo_dict['vm_usage'] = vm_usage/1024
+         vm_reserved = vm.resourcePool.summary.runtime.memory.reservationUsedForVm
+         repo_dict['vm_reserved']=vm_reserved/1024/1024
+       except:
+         pass
        repo_dict['vmware_disklist'] = []
-       hardware = vm.config.hardware
-       for each_vm_hardware in hardware.device:
+       
+       for device in vm.config.hardware.device:
+            if (device.key >= 2000) and (device.key < 3000):
+               tmp_dict = {}
+               if hasattr(device.backing, 'fileName'):
+                   tmp_dict = {}
+                   datastore = device.backing.datastore
+                   if datastore:
+                       tmp_dict['reponame'] = datastore.name
+                       tmp_dict['capacity']  = device.capacityInKB/1024/1024 # converting bytes to GB
+                       #tmp_dict['used_size'] = (datastore.summary.capacity - datastore.summary.freeSpace)/1024/1024/1024
+                       tmp_dict['disk'] = device.backing.fileName.split('/')[1]
+               else:
+                    tmp_dict['reponame'] = ''
+                    tmp_dict['disk'] = ''
+                    tmp_dict['capacity']= 0
+                    tmp_dict['used_size'] = 0
+               repo_dict['vmware_disklist'].append(tmp_dict)
+       result.append(repo_dict)
+       
+       """for each_vm_hardware in hardware.device:
          if (each_vm_hardware.key >= 2000) and (each_vm_hardware.key < 3000):
            tmp_dict = {}
-           reponame = each_vm_hardware.backing.fileName.split(']')[0]
-           tmp_dict['reponame'] = reponame[1:]
-           disk = each_vm_hardware.backing.fileName.split('/')[1]
-           tmp_dict['disk'] = disk
-           tmp_dict['capacity'] = each_vm_hardware.capacityInKB/1024/1024
+           try:
+               reponame = each_vm_hardware.backing.fileName.split(']')[0]
+               tmp_dict['reponame'] = reponame[1:]
+               disk = each_vm_hardware.backing.fileName.split('/')[1]
+               tmp_dict['disk'] = disk
+               tmp_dict['capacity']= each_vm_hardware.capacityInKB/1024/1024 
+           except:
+               tmp_dict['reponame'] = ''  
+               tmp_dict['disk'] = ''
+               tmp_dict['capacity'] =  0
            repo_dict['vmware_disklist'].append(tmp_dict)
-       result.append(repo_dict)
+       result.append(repo_dict)"""
 
    with open('JSON/vmware.json', 'w') as outfile:
         json.dump(result, outfile)
